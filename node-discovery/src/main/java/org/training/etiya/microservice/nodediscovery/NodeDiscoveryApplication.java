96427f70cd4ccbbf404239d5989d51d558dd18f7
ibrahimdoss <72415820+ibrahimdoss@users.noreply.github.com>
2022-09-26 14:52:43 +0300

    configuration

---
+++ node-discovery/src/main/java/org/training/etiya/microservice/nodediscovery/NodeDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package org.training.etiya.microservice.nodediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class NodeDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NodeDiscoveryApplication.class, args);
+    }
+
+}
